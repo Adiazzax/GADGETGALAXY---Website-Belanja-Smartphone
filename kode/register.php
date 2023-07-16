a14f9744fd4f7dd7c6ce01d4bb6787987c02b7be
Adiazzax <101486336+Adiazzax@users.noreply.github.com>
2023-07-16 12:03:06 +0700

    update lagi

--- kode/register.php
+++ kode/register.php
@@ -1,3 +1,40 @@
+
+<!-- koneksi database -->
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "akun";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+// validasi saat register
+if (isset($_POST['register'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm_password'];
+    if ($password !== $confirmPassword) {
+        die("Konfirmasi kata sandi tidak sesuai.");
+    }
+
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+
+    $sql = "INSERT INTO usermasuk (username, password) VALUES ('$username', '$hashedPassword')";
+
+    if ($conn->query($sql) === TRUE) {
+        header("Location: login.php");
+        exit();
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
